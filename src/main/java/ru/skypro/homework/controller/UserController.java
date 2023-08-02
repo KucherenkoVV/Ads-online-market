3eb355c78763610a0819bb34039097fface658c4
KucherenkoVV <kucherenko.vv@mail.ru>
2023-08-02 16:33:53 +0800

    Add UserDto and base controllers with base methods.

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,82 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.UserDto;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(value = "http://localhost:3000")
+@Tag(name = "Пользователи", description = "UserController")
+public class UserController {
+
+    //Первоначально стоят загрушки в методах, потом будет изменено на ResponseEntity
+    @Operation(
+            summary = "Обновление пароля", tags = "Пользователи",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200", description = "OK",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = NewPassword.class))}),
+                    @ApiResponse(responseCode = "401", description = "Unauthorised", content = @Content),
+                    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+            }
+    )
+    @PostMapping("/set_password")
+    public NewPassword setPassword(@RequestBody NewPassword newPassword) {
+        System.out.println("пароль обновлен");
+        return new NewPassword();
+    }
+
+    @Operation(
+            summary = "Получить информацию об авторизованном пользователе", tags = "Пользователи",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200", description = "OK",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class))}),
+                    @ApiResponse(responseCode = "401", description = "Unauthorised", content = @Content)
+            }
+    )
+    @GetMapping("/me")
+    public UserDto getUser(UserDto userDto) {
+        System.out.println("Получена информация о пользователе");
+        return new UserDto();
+    }
+
+    @Operation(
+            summary = "Обновить информацию об авторизованном пользователе", tags = "Пользователи",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200", description = "OK",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDto.class))}),
+                    @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+                    @ApiResponse(responseCode = "401", description = "Unauthorised", content = @Content)
+            }
+    )
+    @PatchMapping("/me")
+    public UserDto updateUser(@RequestBody UserDto userDto) {
+        System.out.println("Обновлена информация о пользователе");
+        return new UserDto();
+    }
+
+    @Operation(
+            summary = "Обновить аватар авторизованного пользователя", tags = "Пользователи",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK", content = @Content),
+                    @ApiResponse(responseCode = "401", description = "Unauthorised", content = @Content)
+            }
+    )
+    @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void updateUserAvatar(@RequestPart("image") MultipartFile multipartFile)  {
+        System.out.println("Обновлен аватар");
+    }
+}
+
